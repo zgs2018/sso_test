6c6022eeefe3d2a4a3c64a902cc76829d18aa5d1
Mrlaozhou <zgs5999@163.com>
2018-06-25 08:48:54 +0800

    change.config.zgs

--- App/Apply/Conf/config.php
+++ App/Apply/Conf/config.php
@@ -3,12 +3,12 @@ return array(
 	//'配置项'=>'配置值'
     'DB_CONFIG_EDU'=>array(
         'DB_TYPE'=>'mysql',
-        'DB_HOST'=>'121.40.67.121',
-        'DB_PORT'=>'3306',
+        'DB_HOST'=>env('DB_HOST'),
+        'DB_PORT'=>env('DB_PORT'),
         'DB_NAME'=>'mxcrm',
-        'DB_USER'=>'root2',
-        'DB_PWD'=>'laozhou',
-        'DB_PREFIX'=>'',
+        'DB_USER'=>env('DB_USER'),
+        'DB_PWD'=>env('DB_PASS'),
+        'DB_PREFIX'=>'mx_',
     ),
     // test
 );
