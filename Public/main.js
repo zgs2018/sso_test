ec4e63973fa2ae8c260bce7ec8db5af858115e23
452757438@qq.com <452757438@qq.com>
2018-06-27 16:20:37 +0800

    add moblie judgment

---
+++ Public/main.js
@@ -0,0 +1,19 @@
+$(function(){
+	function IsPC() {
+        var userAgentInfo = navigator.userAgent;
+        var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPod");
+        var flag = true;
+        for (var v = 0; v < Agents.length; v++) {
+            if (userAgentInfo.indexOf(Agents[v]) > 0) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+
+    if (!IsPC()) {
+        window.location.href = "/m"
+    }
+
+})
